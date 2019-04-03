ae69e4393aaf86f50c56018083cd819b513e2c9f
Labib <labibc01@gmail.com>
2019-04-03 01:14:34 -0400

    end my misery

--- a4/ext2_mkdir.c
+++ a4/ext2_mkdir.c
@@ -38,8 +38,8 @@ int main(int argc, char **argv) {
     // initialize new inode
     init_inode(EXT2_S_IFDIR, free_inode, free_block);
     int root_len = get_rec_len(".");
-    init_dir_entry(free_block, EXT2_FT_DIR, free_inode, ".", root_len); // add ./ dir
-    init_dir_entry(free_block+root_len, EXT2_FT_DIR, parent_inode, "..", EXT2_BLOCK_SIZE-root_len); // add ../ dir
+    init_dir_entry(free_block, 0, EXT2_FT_DIR, free_inode, ".", root_len); // add ./ dir
+    init_dir_entry(free_block, root_len, EXT2_FT_DIR, parent_inode, "..", EXT2_BLOCK_SIZE-root_len); // add ../ dir
     int ret = add_dir_to_parent(parent_inode, free_inode, path_data->file_name);
     if (ret){
         return ENOSPC;
