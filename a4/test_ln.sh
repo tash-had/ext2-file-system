ff4d86900feb2296dcf2db1d6a5d937b2b2eed4b
Tash-had Saqif <me@tash-had.com>
2019-04-05 21:13:39 -0400

    add ln tests

---
+++ a4/test_ln.sh
@@ -0,0 +1,21 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#    LN
+#       TESTS
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+echo ""
+echo ""
+echo ""
+echo "LN TESTS"
+sh reloaddisk.sh
+
+./ext2_ln images/emptydisk.img readimage /readimghardlnk
+read -p "EXT2_CP: COPIED readimage TO /test. READ IMAGE? Y/N " answer
+case ${answer:0:1} in
+    y|Y )
+        ./readimage images/emptydisk.img
+    ;;
+    * )
+        echo "====="
+    ;;
+esac
+echo "====="
