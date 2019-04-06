bd4b2a61b77d3fbaeebcb7a684d269354e027424
Tash-had Saqif <me@tash-had.com>
2019-04-05 21:40:19 -0400

    test rm

---
+++ a4/test_rm.sh
@@ -0,0 +1,35 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#    CP
+#       TESTS
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+echo ""
+echo ""
+echo ""
+echo "RM TESTS"
+sh reloaddisk.sh
+
+
+
+./ext2_cp images/emptydisk.img readimage /
+read -p "EXT2_CP: COPIED readimage TO /. READ IMAGE? Y/N " answer
+case ${answer:0:1} in
+    y|Y )
+        ./readimage images/emptydisk.img
+    ;;
+    * )
+        echo "====="
+    ;;
+esac
+echo "====="
+
+# ./ext2_rm images/emptydisk.img /readimage
+# read -p "EXT2_CP: COPIED readimage TO /. READ IMAGE? Y/N " answer
+# case ${answer:0:1} in
+#     y|Y )
+#         ./readimage images/emptydisk.img
+#     ;;
+#     * )
+#         echo "====="
+#     ;;
+# esac
+# echo "====="
